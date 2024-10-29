e8a1250b6ecbeba8008c6f84da26cc0fda1d7719
CodeOfUsami <zhang0285@foxmail.com>
2024-10-29 19:29:49 +0800

    add:src/utils.py

---
+++ src/utils.py
@@ -0,0 +1,8 @@
+def print_communities(communities):
+    """
+    格式化输出社区划分结果。
+    :param communities:
+    :return:
+    """
+    for i, community in enumerate(communities, 1):
+        print(f"社区{i}:{community}")
